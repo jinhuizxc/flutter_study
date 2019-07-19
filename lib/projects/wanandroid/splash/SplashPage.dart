aa8ee6de67092ad7ac455367fd662d387eb2e178
j_h <13113017500>
2019-07-19 09:15:20 +0800

    周末开始学习, 搞个demo->;

---
+++ lib/projects/wanandroid/splash/SplashPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/*
+ * 欢迎页， 周末开始写完，学习ing;
+ */
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
