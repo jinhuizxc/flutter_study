4fbab6ebfcaedee7c5620e4ea143ab4117420cc4
j_h <13113017500>
2019-12-11 20:18:02 +0800

    欢迎页显示;

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_study/utils/toast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Utils {
@@ -12,7 +13,8 @@ class Utils {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
-      Toast.show('拨号失败！');
+//      Toast.show('拨号失败！');
+      Fluttertoast.showToast(msg: "拨号失败");
     }
   }
 }
